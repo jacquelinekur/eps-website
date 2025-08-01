db8ccc23980346eabdf66d9e994a56b1108631c9
jacquelinekur <jacquelinekurniawan02@gmail.com>
2025-08-01 22:11:05 +0700

    contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import cover_img from '../assets/assets-about/__W_9571-Edit 1.png'
 import img2 from '../assets/assets-about/__W_9598-Edit 1.png'
-import '../css/about.css'
+import '../css/pages.css'
 
 function About() {
   return (
     <div>
       <img id='cover-img' src={cover_img} alt="" />
-      <div className='about-section'>
-        <h4 className='about-heading'>About Us</h4>
-        <p className='about-text'>EPS was founded in 2013 by Indrawati Sumadi and Lily Ang. We are a general contractor company for commercial and private residentail projects.
+      <div className='pages-section'>
+        <h4 className='pages-heading'>About Us</h4>
+        <p className='pages-text'>EPS was founded in 2013 by Indrawati Sumadi and Lily Ang. We are a general contractor company for commercial and private residentail projects.
           <br /><br />
           We aim to build comfortable homes and residences, to cater to clients' individual needs, and to prioritize 
           quality in all that we do.
@@ -20,15 +20,15 @@ function About() {
       <div className='about-img-container'>
         <img id='about-img2' src={img2} alt="" />
       </div>
-      <div className='about-section'>
-        <h4 className='about-heading'>Mission</h4>
-        <p className='about-text'>
+      <div className='pages-section'>
+        <h4 className='pages-heading'>Mission</h4>
+        <p className='pages-text'>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia odit pariatur commodi consequatur similique minima a fugit ratione iste dolore.
         </p>
       </div>
-      <div className='about-section'>
-        <h4 className='about-heading'>Vision</h4>
-        <p className='about-text'>
+      <div className='pages-section'>
+        <h4 className='pages-heading'>Vision</h4>
+        <p className='pages-text'>
           To become a leading building and design company in Indonesia, delivering exceptional quality and innovative 
           design for residential, commercial, and office spaces.
         </p>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import cover_img from '../assets/Contact/Contact.png'
+import arrow from '../assets/arrow.png'
+import '../css/pages.css'
+
 
 function Contact() {
   return (
     <div>
-      
+      <img id='cover-img' src={cover_img} alt="" />
+      <div className='pages-section'>
+        <h4 className='pages-heading'>Contact Us</h4>
+        <div className='contact-container'>
+          <p className='contact-text'>
+            Phone Number
+            <br />
+            adm.epsv@gmail.com
+            <br /><br />
+            JL. Panglima Polim IV No. 42 <br />
+            Melawai, Kebayoran Baru, Jakarta Selatan
+          </p>
+          <form className='form-container' action="">
+            <input className='form-input' type="text" placeholder='Name' />
+            <input className='form-input' type="email" placeholder='Email' />
+            <textarea className='form-txtArea' placeholder='Message'/>
+            <div className='btn-container'>
+              <button id='send-btn'>SEND <img id='arrow-img' src={arrow} alt="" /></button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
