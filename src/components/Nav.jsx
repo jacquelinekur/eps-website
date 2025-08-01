e3d865c76a3c812b64ed6e98d281427ac6156e8a
jacquelinekur <jacquelinekurniawan02@gmail.com>
2025-08-01 19:41:34 +0700

    change nav bar colour in projects page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/eps_white.png'
+import { Link, useLocation } from 'react-router-dom'
+import logo_white from '../assets/eps_white.png'
+import logo_black from '../assets/eps-black.png'
+
 
 
 function Nav() {
+  const location = useLocation();
+  const isProjectsPage = location.pathname === '/projects';
+  const navClassName = isProjectsPage ? 'nav-project' : 'nav';
+  const linkClass = isProjectsPage ? 'link-project' : 'link';
+  const logo = isProjectsPage ? logo_black : logo_white;
+
   return (
     <>
-      <nav className='nav'>
+      <nav className={navClassName}>
         <Link to='/'><img id='nav-logo' src={logo} alt="" /></Link>
         <ul>
-            <li>
-                <Link className='link' to='/about'>ABOUT</Link>
-            </li>
-            <li>
-                <Link className='link' to='/projects'>PROJECTS</Link>
-            </li>
-            <li>
-                <Link className='link' to='/contact'>CONTACT</Link>
-            </li>
+          <li>
+            <Link className={linkClass} to='/about'>ABOUT</Link>
+          </li>
+          <li>
+            <Link className={linkClass}  to='/projects'>PROJECTS</Link>
+          </li>
+          <li>
+            <Link className={linkClass} to='/contact'>CONTACT</Link>
+          </li>
         </ul>
       </nav>
     </>
